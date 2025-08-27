f3ef8297e9b6238e78e103b14f735d2c8fa4b0c8
ma-muktadeer <muktadeer.ma@gmail.com>
2025-08-27 10:43:52 +0600

    1st init

---
+++ ui/mshop/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  appVersion: '1.0.0',
+  production: true,
+  disableRightClick: false,
+  // SERVER_URL:"http://172.25.204.232:8080/rbs-server",
+  // SERVER_BASE_URL:"http://172.25.204.232:8080/rbs-server/secure/api/v1",
+  // SERVER_BASE_URL_PUBLIC: "http://172.25.204.232:8080/rbs-server/public/api/v1",
+  // SERVER_BASE_URL_ADMIN: "http://172.25.204.232:8080/rbs-server/secure/api/v1",
+  SECRET_KEY : 'softcafe'
+};
