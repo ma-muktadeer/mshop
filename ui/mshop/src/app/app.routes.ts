481e62f9a3f48e564686cf2b243d5e7d4b1c6e8c
ma-muktadeer <muktadeer.ma@gmail.com>
2025-08-29 01:12:58 +0600

    login ok

--- ui/mshop/src/app/app.routes.ts
+++ ui/mshop/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Structure } from './theme/structure/structure';
-import { AuthGuard } from './ithouse/guard/auth.guard';
 import { Login } from './components/public/login/login';
 import { loginGuard } from './components/public/login/login.guard';
 import { Dashbord } from './components/secure/dashbord/dashbord';
