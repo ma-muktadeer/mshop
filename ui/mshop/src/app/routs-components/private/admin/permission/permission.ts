46163346c99864b37f9325ec45c8d54eccb68670
ma-muktadeer <muktadeer.ma@gmail.com>
2025-10-14 22:07:43 +0600

    permission add

---
+++ ui/mshop/src/app/routs-components/private/admin/permission/permission.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ithouse-permission',
+  imports: [],
+  templateUrl: './permission.html',
+  styleUrl: './permission.scss'
+})
+export class Permission {
+
+}
