4e3f5b9f3d4bca93af496c4d262afa6052dd707f
ma-muktadeer <muktadeer.ma@gmail.com>
2025-10-15 20:27:52 +0600

    permission sg

--- ui/mshop/src/app/routs-components/private/admin/permission/permission.ts
+++ ui/mshop/src/app/routs-components/private/admin/permission/permission.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ActionType } from 'src/app/ithouse/constants/action-type.enum';
 import { CommonService } from 'src/app/services/common.service';
 import { ContentType } from 'src/app/services/common/constants/content-type.enum';
@@ -9,17 +9,16 @@ import { CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem } from 
 import { FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { Toast } from 'src/app/services/common/Toast';
+import { AsyncDetection, NgScrollbar } from "ngx-scrollbar";
 
 @Component({
   selector: 'ithouse-permission',
-  imports: [DragDropModule, FormsModule],
+  imports: [DragDropModule, FormsModule, AsyncDetection, NgScrollbar],
   templateUrl: './permission.html',
   styleUrl: './permission.scss'
 })
 export class Permission extends Ithouse implements Service {
   filteredPermissionList = signal<any[]>([]);
-  unassignRoleList = signal<any[]>([]);
-
   permissionList: any[] = [];
   clickedItem: any;
   displayName: string;
@@ -32,6 +31,20 @@ export class Permission extends Ithouse implements Service {
   disableAddSave: boolean = false;
   spinnerAddSave: boolean = false;
   user: any;
+  searchRoleText = signal<string>('');
+
+  unassignPermissionListAll = []
+
+  unassignRoleList = computed(() => {
+    const searchText = this.searchRoleText();
+    if (!searchText) {
+      return this.unassignPermissionListAll;
+    }
+    return this.unassignPermissionListAll.filter(x =>
+      x.displayName.toUpperCase().includes(searchText.toUpperCase())
+    );
+  });
+
   constructor(public permissionStoreService: PermissionStoreService,
     private cs: CommonService
   ) {
@@ -45,23 +58,7 @@ export class Permission extends Ithouse implements Service {
   onLoad() {
     this.cs.sendRequestAdmin(this, ActionType.SELECT, ContentType.AppPermission, 'FindAll', {});
   }
-  unassignPermissionListAll = []
-  onSearchPermission(e, terget) {
-    debugger
-    console.log(e)
-    if (terget == 'UN') {
-      if (!e.target.value) {
-        // this.unassignRoleList = this.unassignPermissionListAll
-        this.unassignRoleList.set(this.unassignPermissionListAll);
-      }
-      else {
-        // this.unassignRoleList = this.unassignPermissionListAll.filter(x =>
-        this.unassignRoleList.set(this.unassignPermissionListAll.filter(x =>
-          x.displayName.toUpperCase().indexOf(e.target.value.toUpperCase()) > -1
-        ));
-      }
-    }
-  }
+
   onEdit(e, group) {
     debugger
     this.permissionId = group.permissionId
@@ -161,7 +158,7 @@ export class Permission extends Ithouse implements Service {
 
       var roleGroup = res.payload
       this.assignRoleList = roleGroup.roleList;
-      this.unassignRoleList.set(roleGroup.unassignRoleList);
+      // this.unassignRoleList.set(roleGroup.unassignRoleList);
       // this.unassignRoleList = roleGroup.unassignRoleList;
       this.unassignPermissionListAll = roleGroup.unassignRoleList;
 
@@ -170,10 +167,8 @@ export class Permission extends Ithouse implements Service {
         Swal.fire({ title: "Successfully APPROVE PERMISSION", toast: true, timer: 1000 });
         this.user = res.payload[0];
         this.assignRoleList = this.user.roleList;
-        this.unassignRoleList.set(this.user.unassignRoleList);
-        // this.unassignRoleList = this.user.unassignRoleList;
-        this.unassignRoleList.set(this.unassignRoleList().filter(x => x.status == 'APPROVED'));
-        // this.unassignRoleList = this.unassignRoleList.filter(x => x.status == 'APPROVED');
+        // this.unassignRoleList.set(this.user.unassignRoleList);
+        // this.unassignRoleList.set(this.unassignRoleList().filter(x => x.status == 'APPROVED'));
         // this.filteredUnassignRoleList = this.unassignRoleList
       }
     } else if (res.header.referance == 'APPROVE_DEASSIGN_PERMISSION') {
@@ -181,10 +176,8 @@ export class Permission extends Ithouse implements Service {
         Swal.fire({ title: "Successfully De-Assigned Role", toast: true, timer: 1000 });
         this.user = res.payload[0];
         this.assignRoleList = this.user.roleList;
-        this.unassignRoleList.set(this.user.unassignRoleList);
-        // this.unassignRoleList = this.user.unassignRoleList;
-        this.unassignRoleList.set(this.unassignRoleList().filter(x => x.status == 'APPROVED'));
-        // this.unassignRoleList = this.unassignRoleList.filter(x => x.status == 'APPROVED');
+        // this.unassignRoleList.set(this.user.unassignRoleList);
+        // this.unassignRoleList.set(this.unassignRoleList().filter(x => x.status == 'APPROVED'));
         // this.filteredUnassignRoleList = this.unassignRoleList
       }
     }
