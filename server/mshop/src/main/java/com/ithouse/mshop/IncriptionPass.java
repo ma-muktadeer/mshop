799983433e5a33fa4a566eed20725dfd7b346406
ma-muktadeer <muktadeer.ma@gmail.com>
2025-11-23 10:58:25 +0600

    ok

--- server/mshop/src/main/java/com/ithouse/mshop/IncriptionPass.java
+++ server/mshop/src/main/java/com/ithouse/mshop/IncriptionPass.java
@@ -6,6 +6,6 @@ public class IncriptionPass {
 
     public static void main(String[] args) throws Exception {
         var s = EncryptDecryptService.encrypt("esig-123");
-        IO.println("Encrypt data: " + s);
+        //IO.println("Encrypt data: " + s);
     }
 }
