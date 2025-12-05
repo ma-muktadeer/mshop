fb94b270352ca929c174cf66622f0e2df3150f6c
ma-muktadeer <muktadeer.ma@gmail.com>
2025-12-05 21:13:50 +0600

    ok

--- server/mshop/src/main/java/com/ithouse/mshop/core/principal/UserPrincipalService.java
+++ server/mshop/src/main/java/com/ithouse/mshop/core/principal/UserPrincipalService.java
@@ -1,22 +1,16 @@
 package com.ithouse.mshop.core.principal;
 
-import com.ithouse.mshop.core.entity.AppPermission;
 import com.ithouse.mshop.core.entity.Role;
 import com.ithouse.mshop.core.entity.User;
 import com.ithouse.mshop.core.repository.UserRepo;
 import com.ithouse.mshop.core.service.AppPermissionService;
-import com.ithouse.mshop.core.service.UserService;
 import jakarta.transaction.Transactional;
 import org.hibernate.Hibernate;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class UserPrincipalService implements UserDetailsService {
@@ -33,12 +27,12 @@ public class UserPrincipalService implements UserDetailsService {
     @Transactional
     public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
         User user;
-        if(username.equals("ithousebd") || username.equals("ithousebd@admin.com")) {
+        if (username.equals("ithousebd") || username.equals("ithousebd@admin.com")) {
             user = userRepo.findAllByLoginNameOrEmailAndActive(username, username, 1)
                     .orElseThrow(() -> new UsernameNotFoundException("User not found."));
-        }else {
+        } else {
             user = userRepo
-                    .findAllByLoginNameOrEmailAndAppNameAndActive(username, username, "M-SHOP", 1 )
+                    .findAllByLoginNameOrEmailAndAppNameAndActive(username, username, "M-SHOP", 1)
                     .orElseThrow(() -> new UsernameNotFoundException("User not found."));
         }
 
