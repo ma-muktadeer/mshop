c523bbd42b1bb5a02cf94e48f566e3d71a84eed6
ma-muktadeer <muktadeer.ma@gmail.com>
2025-12-12 23:38:59 +0600

    ok

--- server/mshop/src/main/java/com/ithouse/mshop/core/provider/ItHouseDBValueProvider.java
+++ server/mshop/src/main/java/com/ithouse/mshop/core/provider/ItHouseDBValueProvider.java
@@ -6,6 +6,8 @@ import com.ithouse.core.anotations.provider.ItHouseConfigProvider;
 import com.ithouse.core.anotations.services.ItHouseDBValueService;
 import com.ithouse.mshop.core.entity.SConfiguration;
 import com.ithouse.mshop.core.service.SConfigurationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 
 @Configuration
 public class ItHouseDBValueProvider {
+    private static final Logger log = LoggerFactory.getLogger(ItHouseDBValueProvider.class);
     private final SConfigurationService sConfigurationService;
     Map<String, SConfiguration> configMap = new HashMap<>();
 
@@ -25,6 +28,7 @@ public class ItHouseDBValueProvider {
     }
 
     public void setsConfigurations() {
+        try{
         List<SConfiguration> configurations = sConfigurationService.findAllByConfigGroupAndActive("APP_CONFIG_GROUP", 1);
         configMap = configurations.stream()
                 .collect(Collectors.toMap(
@@ -32,6 +36,10 @@ public class ItHouseDBValueProvider {
                         m -> m,
                         (a, b) -> b
                 ));
+        }catch (Exception e){
+            log.error("Getting error=>{}", e.getMessage());
+//            throw e;
+        }
 
     }
 
