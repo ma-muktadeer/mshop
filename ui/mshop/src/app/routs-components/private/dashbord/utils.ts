de12e62f0a42a36ff9e5238d518f258cf48ed863
ma-muktadeer <muktadeer.ma@gmail.com>
2025-12-15 10:45:00 +0600

    ok

---
+++ ui/mshop/src/app/routs-components/private/dashbord/utils.ts
@@ -0,0 +1,13 @@
+
+export type DeepPartial<T> = { [P in keyof T]?: _DeepPartial<T[P]> };
+
+/** @private */
+export type _DeepPartial<T> = T extends Function
+  ? T
+  : T extends Array<infer U>
+  ? _DeepPartialArray<U>
+  : T extends object
+  ? DeepPartial<T>
+  : T | undefined;
+/** @private */
+export interface _DeepPartialArray<T> extends Array<_DeepPartial<T>> { }
