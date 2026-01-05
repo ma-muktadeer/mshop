dac872eadeb68c5b35a1e3fed252f59fbd5a8548
M A MUKTADEER <muktadeer.ma@gmail.com>
2026-01-05 22:23:24 +0600

    ok

--- ui/mshop/src/app/ithouse/common/ithouse-grid/ithouse-grid-module.ts
+++ ui/mshop/src/app/ithouse/common/ithouse-grid/ithouse-grid-module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { IthouseGridBody } from './ithouse-grid-body/ithouse-grid-body';
 import { IthouseGridPagination } from './ithouse-grid-pagination/ithouse-grid-pagination';
 import { IthouseGrid } from './ithouse-grid/ithouse-grid';
