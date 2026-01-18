7a99f6f91d3e3e372292077c927fddb2e0c3bcfc
M A MUKTADEER <muktadeer.ma@gmail.com>
2026-01-18 23:28:16 +0600

    login update

--- ui/mshop/src/app/routs-components/public/public-routs.ts
+++ ui/mshop/src/app/routs-components/public/public-routs.ts
@@ -1,8 +1,12 @@
 import { Routes } from "@angular/router";
 import { Check } from "./check/check";
+import { PageNotFound } from "./page-not-found/page-not-found";
 
 export const publicRoutes: Routes = [
   {
     path: 'check', component: Check, pathMatch: 'full',
   },
+  {
+    path: 'page-not-found', component: PageNotFound, pathMatch: 'full',
+  }
 ];
